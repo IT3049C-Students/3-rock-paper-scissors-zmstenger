3d0f465fecdbcbbb80af62b5a0865faea11b34b8
Zach <stengezm@mail.uc.edu>
2022-06-17 16:01:38 -0400

    commit

--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -14,34 +14,66 @@ class RockPaperScissors {
    */
   generateCPUResponse(){
     const acceptedValues = [ `rock`, `paper`, `scissors` ];
-
-    return ;
+    return acceptedValues[Math.floor(Math.random() * 3)];
   }
   /**
-   * returns one of the following values: `win`, `lose`, `tie`
-   * tie:
-   *     the user selection the same as the CPU
-   * win: 
-   *    (user is `rock` and cpu is `scissors
-   *     OR
-   *    (user is `paper` and cpu is `rock`) 
-   *     OR 
-   *    (user is `scissors` and cpu is `paper`)
-   * `lose`:
-   *    the opposite case :)
    * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    * @param {string} cpuSelection computer selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   determineWinner(userSelection, cpuSelection){
+    if (userSelection === cpuSelection) {
+      return "tie";
+    }
 
+    if (userSelection === "rock") {
+      if (cpuSelection === "scissors") {
+        return "win"
+      } else {
+        return "lose"
+      }
+    }
+    //Check for Paper
+    else if (userSelection === "paper") {
+      if (cpuSelection === "rock") {
+       return "win"
+      } else {
+        return "lose"
+      }
+    }
+    //Check for Scissors
+    else if(userSelection == "scissors"){
+      if (cpuSelection === "paper") {
+        return "win"
+      } else {
+        return "lose"
+      }
+    }
+
+  else{
+    return "lose"
   }
-
+} 
   /**
-   * 
    * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   play(userSelection){
+    const cpuResponse = this.generateCPUResponse()
+    if(this.determineWinner(userSelection, cpuResponse) == "win")
+    {
+      this.score.user++
+      this.gameHistoryLog.push(`${this.username} selected ${userSelection}, CPU selected ${cpuResponse}: ${this.username} wins`);
 
+    }
+    else if(this.determineWinner(userSelection, cpuResponse)=="lose")
+    {
+      this.score.cpu++
+      this.gameHistoryLog.push(`${this.username} selected ${userSelection}, CPU selected ${cpuResponse}: CPU wins`);
+
+    }
+    else if(this.determineWinner(userSelection, cpuResponse)=="tie")
+    {
+      this.gameHistoryLog.push(`${this.username} selected ${userSelection}, CPU selected ${cpuResponse}: ${this.username} ties with CPU`);
+    }
   }
 
 }
